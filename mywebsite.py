68e82b9115e1b840c994a287f76b50afe41a01dd
hyerynn0521 <52299631+hyerynn0521@users.noreply.github.com>
2019-07-10 15:20:04 -0700

    Create mywebsite.py

---
+++ mywebsite.py
@@ -0,0 +1,64 @@
+<!-- πanissimo
+
+Main interests
+Piano
+Check out videos of me practicing various songs:
+Another day of sun
+Moonlight sonata
+flute/piano duets
+
+Math
+Here are some links to some good math youtube channels:
+3blue1brown
+
+
+Coding
+To those of you who are going up to 11th or 12th grade, apply to Girls who code programs! -->
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link
+    rel="stylesheet"
+    href="website.css">
+  </head>
+  <body>
+    <p>
+      <title><h1><em><strong>pi</strong></em>ANISSIMO</h1></title>
+    </p>
+    <div>
+      <h2><center>Welcome to my web page!</center></h2>
+    </div>
+    <div>
+    <center><img src="selfie.jpg" length="300" width="105"></center>
+    </div>
+    <div>
+      <center><h3>math, music, coding!! <div>I have been wating to go into a medical field, but I recently changed
+      my mind becuase of <a href="https://girlswhocode.com/"> Girls Who Code!</a></div>
+      I have been playing piano for 10 years:) <p>do you want to see me play?</p></h3></center>
+    </div>
+    <div>
+    </div>
+    <p>
+      <center><button type="button" onclick="document.getElementById('demo').style.display='block'">yes</button>
+      <p id="demo" style="display:none">I suck tho so you might not want to.. do you really want to?
+        <button type="button" onclick="document.getElementById('demo2').style.display='block'">yes</button>
+        <button type="button" onclick="document.getElementById('demo3').style.display='block'">no</button></p>
+      <button type="button" onclick="document.getElementById('demo1').style.display='block'">no</button>
+      <p id="demo1" style="display:none"><a href="https://www.instagram.com/_hyerinlee/">click here</a></p></center>
+      <center><p id="demo2" style="display:none"><a href="website2.html">click here</a></p>
+      <p id="demo3" style="display:none">Good job, you chose the right option!</p></center>
+
+    </p>
+    <p>
+      <center><h3>Also, check out my favorite math youtube channel by clicking on the logo:</h3></center>
+    </p>
+    <center><a href="https://www.youtube.com/channel/UCYO_jab_esuFRV4b17AJtAw">
+      <img src="3blue1brown.png" length = "100" width="100"></a></center>
+    <script src="website.js"></script>
+
+
+
+
+    <p><h6>Hey, if you read this click <a href="mywebsite1.html">here</a></h6></p>
+  </body>
+</html>
